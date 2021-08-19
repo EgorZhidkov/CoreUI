4b29465fc3eac65bbb98ea7ce08499121ab4d32c
Egor Zhidkov <e.jidkov@gmail.com>
2021-08-19 14:36:59 +0300

    Save user profile in Vuex

--- src/store.js
+++ src/store.js
@@ -3,25 +3,46 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 const state = {
-  sidebarShow: 'responsive',
-  sidebarMinimize: false
+    user: {
+        username: '',
+        password: '',
+        name: '',
+        role: '',
+    },
+    sidebarShow: 'responsive',
+    sidebarMinimize: false
 }
 
 const mutations = {
-  toggleSidebarDesktop (state) {
-    const sidebarOpened = [true, 'responsive'].includes(state.sidebarShow)
-    state.sidebarShow = sidebarOpened ? false : 'responsive'
-  },
-  toggleSidebarMobile (state) {
-    const sidebarClosed = [false, 'responsive'].includes(state.sidebarShow)
-    state.sidebarShow = sidebarClosed ? true : 'responsive'
-  },
-  set (state, [variable, value]) {
-    state[variable] = value
-  }
+    toggleSidebarDesktop(state) {
+        const sidebarOpened = [true, 'responsive'].includes(state.sidebarShow)
+        state.sidebarShow = sidebarOpened ? false : 'responsive'
+    },
+    toggleSidebarMobile(state) {
+        const sidebarClosed = [false, 'responsive'].includes(state.sidebarShow)
+        state.sidebarShow = sidebarClosed ? true : 'responsive'
+    },
+    set(state, [variable, value]) {
+        state[variable] = value
+    },
+    setUser(state, payload) {
+        state.user.username = payload.username;
+        state.user.password = payload.password;
+        state.user.name = payload.name;
+        state.user.role = payload.role;
+
+    }
+}
+
+const getters = {
+    getUserInfo(state) {
+        let userObjJson = JSON.stringify(state.user);
+        console.log("hehe", userObjJson);
+    }
 }
 
 export default new Vuex.Store({
-  state,
-  mutations
+    state,
+    mutations,
+    getters
 })
--- src/views/pages/storageUser.js
+++ src/views/pages/storageUser.js
@@ -1,6 +1,6 @@
 const storageUser = [
-    { username: 'Egor', password: '123' },
-    { username: 'Diana', password: '345' },
-    { username: 'Alex', password: '567' }
+    { username: 'Egor', password: '123', name: 'Egor', role: 'admin' },
+    { username: 'Diana', password: '345', name: 'Egor', role: 'user' },
+    { username: 'Alex', password: '567', name: 'Egor', role: 'user' }
 ]
 export default storageUser;
